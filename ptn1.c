24be560ba29d2a5d26b3b4168347e564b0eb6302
bhavdip sisara <161108566+bhavdipsisara@users.noreply.github.com>
2024-03-02 20:31:34 +0530

    Add files via upload

---
+++ ptn1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("-------\n");
+	printf("|\t|\n");
+	printf("r\t|\n");
+	printf("n\t|\n");
+	printf("w\t|\n");
+	printf("|\t|\n");
+	printf("-------\n");
+}
---
+++ self.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+
+{
+	printf("my name:bhavdip sisara\n");
+	printf("my age: 19\n");
+	printf("my collge name:gec modasa");
+}
